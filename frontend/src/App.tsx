1a7dc662f06fcc9a900cd3adddc3a5380d9aae0c
Nilton <niltoneapontes@gmail.com>
2024-01-01 14:45:30 -0300

    feat: adding firebase

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,20 @@
 import React from 'react'
 import { RouterProvider } from 'react-router-dom'
 import routes from './routes/routes'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIcjnxnoVZXlQZJtmD_zyOdL3Sb7VidKQ",
+  authDomain: "mybujo-399813.firebaseapp.com",
+  databaseURL: "https://mybujo-399813-default-rtdb.firebaseio.com",
+  projectId: "mybujo-399813",
+  storageBucket: "mybujo-399813.appspot.com",
+  messagingSenderId: "383023240379",
+  appId: "1:383023240379:web:4e06b41298b3de7c25c59e",
+  measurementId: "G-7343E9RTVW"
+};
+
+const app = initializeApp(firebaseConfig);
 
 function App() {
 
