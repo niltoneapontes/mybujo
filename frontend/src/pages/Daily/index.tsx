066f26f973104fd34dff9f1d9c1474ec13449b1f
Nilton <niltoneapontes@gmail.com>
2024-01-01 22:47:17 -0300

    feat: adding rich editor

--- frontend/src/pages/Daily/index.tsx
+++ frontend/src/pages/Daily/index.tsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import Sidebar from '../../components/Sidebar';
 import { db } from '../../App';
 import { collection, getDocs } from 'firebase/firestore';
@@ -8,6 +8,8 @@ import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 
 function Daily() {
+  const editorRef = useRef<ReactQuill>(null);
+
   const today = new Date()
   const todayFormatted = new Date(today.getFullYear(), today.getMonth(), today.getDate())
   .toISOString()
@@ -36,15 +38,40 @@ function Daily() {
   fetchPost()
   }, [])
 
+  const insertCheckbox = () => {
+    const editor = editorRef.current!!.getEditor();
+    const range = editor.getSelection(true);
+    const textToInsert = '<input type="checkbox"/>';
+    editor.insertText(range.index, textToInsert);
+    editor.setSelection(range.index, textToInsert.length);
+  };
+
+
   return (
     <div className='w-full bg-white'>
       <Sidebar></Sidebar>
-      <div className='ml-72 h-screen' >
+      <div className='ml-64 h-screen' >
       <ReactQuill
+      ref={editorRef}
         theme="snow"
         value={content}
         onChange={handleChange}
         className='bg-white'
+        modules={{
+          toolbar: {
+            container: [
+              [{ header: [4, false] }],
+              ['bold', 'italic', 'underline', 'strike'],
+              [{ list: 'ordered' }, { list: 'bullet' }],
+              ['clean'],
+              [{ 'custom': 'customButton' }],
+            ],
+            handlers: {
+              customButton: insertCheckbox,
+            },
+          }
+        }
+        }
       />
       </div>
     </div>
